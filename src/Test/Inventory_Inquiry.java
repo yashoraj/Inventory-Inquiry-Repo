57c438de84f0429b76bb4be6b3381f64de2ac2f4
yashoraj <47247520+yashoraj@users.noreply.github.com>
2019-02-04 07:05:25 -0700

    Update Inventory_Inquiry.java

--- src/Test/Inventory_Inquiry.java
+++ src/Test/Inventory_Inquiry.java
@@ -42,6 +42,9 @@ public class Inventory_Inquiry {
 	};
 	public static String timestamp=new SimpleDateFormat("yyyy-MM-dd'_T_'HH_mm_ss").format(new Date());
 	public Inventory_Inquiry() {
+		
+		
+		int y= 0;
 	}
 	@Test(priority=1)	
 	public static void TC001_Verify_BasicSearch() throws Exception{
